55a447f1c6445d0b970d03dea9ce509e8c923dfa
heyang <1735175988@qq.com>
2023-11-27 19:55:16 +0800

    完成密码登录，验证码登录和注册界面

---
+++ src/router/index.ts
@@ -0,0 +1,36 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+import ChatRoomView from '../views/ChatRoomView.vue'
+import LoginView from '../views/LoginView.vue'
+import SignupView from '../views/SignupView.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'login',
+      component: LoginView
+    },
+    {
+      path: '/signup',
+      name: 'signup',
+      component: SignupView
+    },
+    {
+      path: '/chat',
+      name: 'chat',
+      component: ChatRoomView
+    },
+    {
+      path: '/about',
+      name: 'about',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/AboutView.vue')
+    }
+  ]
+})
+
+export default router
---
+++ src/views/LoginView.vue
@@ -0,0 +1,151 @@
+<template>
+  <div style="width: 100vw; height: 100vh; margin-left: -60px;">
+    <!-- 其他组件内容 -->
+
+    <!-- iframe引入loginsign.html -->
+    <iframe 
+      src="/LoginSignupPage.html" 
+      style="width: 100%; height: 100%; border: none;"
+      id="myIframe"
+    ></iframe>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MyComponent',
+  data(){
+    return{
+      isLoginPage: 1,  // 1表示在直接密码登录， 0表示在验证码登录
+    }
+    
+  },
+  // 组件逻辑
+  methods: {
+
+  setupIframeListener() {
+    const iframe = document.getElementById('myIframe');
+    iframe.onload = () => {
+      const iframeDocument = iframe.contentDocument || iframe.contentWindow.document;
+      const signInButton = iframeDocument.getElementById("form2");
+      const signUpButton = iframeDocument.getElementById("form1");
+      const sendCodeButton = iframeDocument.getElementById("send-code-button")
+      signInButton.addEventListener('submit', this.handleSignIn);
+      signUpButton.addEventListener('submit', this.handleSignUp);
+      sendCodeButton.addEventListener('click', this.handleSendCode)
+    };
+  },
+  handleSignUp(e){
+    e.preventDefault();
+    alert(this.getSignupUserFromIframe())
+    alert(this.getSignupEmailFromIframe())
+    alert(this.getSignupPasswordFromIframe())
+    // 这里可以执行后续操作，比如使用 Axios 发送请求
+  },
+  handleSignIn(e) {
+    e.preventDefault();
+
+    if (this.getSigninStatusFromIframe() === 'login'){
+      alert(this.getSigninEmailFromIframe())
+      alert(this.getSigninPasswordFromIframe())
+    }
+    else{
+      alert(this.getSigninEmailFromIframe())
+      alert(this.getSigninVerifyCodeFromIframe())
+    }
+
+
+    // 这里可以执行后续操作，比如使用 Axios 发送请求
+  },
+  handleSendCode(){
+    alert("send_code")
+  },
+
+  sendSignInRequest() {
+    // 获取输入数据，例如邮箱和密码
+    const email = this.getSigninEmailFromIframe();
+    const password = this.getSigninPasswordFromIframe();
+
+    // 使用 Axios 发送请求
+    axios.post('YOUR_SERVER_ENDPOINT', {
+      email: email,
+      password: password
+    })
+    .then(response => {
+      console.log(response);
+      // 处理响应
+    })
+    .catch(error => {
+      console.error(error);
+      // 处理错误
+    });
+  },
+
+
+
+  // 下面都是获取数据
+  getSigninEmailFromIframe() {
+    const iframe = document.getElementById('myIframe');
+    const iframeDocument = iframe.contentDocument || iframe.contentWindow.document;
+    const emailInput = iframeDocument.querySelector('.container--signin input[type="phone"]');
+    return emailInput ? emailInput.value : null;
+  },
+  getSigninVerifyCodeFromIframe() {
+    const iframe = document.getElementById('myIframe');
+    const iframeDocument = iframe.contentDocument || iframe.contentWindow.document;
+    const emailInput = iframeDocument.querySelector('.container--signin .verify-code-input');
+    return emailInput ? emailInput.value : null;
+  },
+  getSigninStatusFromIframe(){
+    const iframe = document.getElementById('myIframe');
+    const iframeDocument = iframe.contentDocument || iframe.contentWindow.document;
+    const emailInput = iframeDocument.querySelector('.container--signin .status');
+    return emailInput ? emailInput.value : null;
+  },
+
+  getSigninPasswordFromIframe() {
+    const iframe = document.getElementById('myIframe');
+    const iframeDocument = iframe.contentDocument || iframe.contentWindow.document;
+    const passwordInput = iframeDocument.querySelector('.container--signin input[type="password"]');
+    return passwordInput ? passwordInput.value : null;
+  },
+  getSignupUserFromIframe() {
+    const iframe = document.getElementById('myIframe');
+    const iframeDocument = iframe.contentDocument || iframe.contentWindow.document;
+    const passwordInput = iframeDocument.querySelector('.container--signup input[type="text"]');
+    return passwordInput ? passwordInput.value : null;
+  },
+  getSignupEmailFromIframe() {
+    const iframe = document.getElementById('myIframe');
+    const iframeDocument = iframe.contentDocument || iframe.contentWindow.document;
+    const passwordInput = iframeDocument.querySelector('.container--signup input[type="phone"]');
+    return passwordInput ? passwordInput.value : null;
+  },
+  getSignupPasswordFromIframe() {
+    const iframe = document.getElementById('myIframe');
+    const iframeDocument = iframe.contentDocument || iframe.contentWindow.document;
+    const passwordInput = iframeDocument.querySelector('.container--signup input[type="password"]');
+    return passwordInput ? passwordInput.value : null;
+  },
+  
+
+},
+
+
+
+mounted() {
+  this.setupIframeListener();
+}
+
+}
+</script>
+
+<style>
+  body, html {
+    margin: 0;
+    padding: 0;
+    height: 100vh;
+    width: 100vw;
+    overflow: hidden; /* 如果您不想页面出现滚动条 */
+  }
+</style>
